d29aa5058d708214b833b18804558b4c88f544a7
Kiyiko <Kiyiko@gmail.com>
2015-08-23 23:51:16 -0500

    Added auto-kick on pfsense

--- src/main2.py
+++ src/main2.py
@@ -36,11 +36,29 @@ def sanityCheck(packet):
 def dec2ip(ip):
 	ip2 = struct.pack("<L", ip)
 	return ipaddress.ip_address(ip2)
+
+def colourReplace(string):
+	newstring = string.replace("^1", "\033[1;31m")#red
+	newstring = newstring.replace("^2", "\033[1;32m")#green
+	newstring = newstring.replace("^3", "\033[1;33m")#yellow
+	newstring = newstring.replace("^4", "\033[1;34m")#blue
+	newstring = newstring.replace("^5", "\033[1;36m")#cyan
+	newstring = newstring.replace("^6", "\033[1;35m")#pink
+	newstring = newstring.replace("^7", "\033[1;37m")#white
+	newstring = newstring.replace("^8", "\033[1;m")#black
+	newstring = newstring.replace("^9", "\033[1;30m")#grey
+	newstring = newstring.replace("^0", "\033[1;m")#black
+	if string != newstring:
+		return newstring + "\033[1;m"
+	else:
+		return string
+
 vac = {}
-def vacCheck(steamID):
+
+def vacCheck(steamID, ip):
 	if vac.get(steamID) is None:
 		vac[steamID] = {"data":"", "time":0}
-	if vac.get(steamID).get("time") > time.time() - 3600:
+	if vac.get(steamID).get("time") < time.time() - 3600:
 		steamKey = open('steamAPI.txt').read()
 		url = "http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=%s&steamids=%s"%(steamKey, steamID)
 		req = request.urlopen(url)
@@ -51,33 +69,37 @@ def vacCheck(steamID):
 			vac[steamID]["time"] = time.time()
 			return "False"
 		else:
-			vac[steamID]["data"] = "%d VAC ban(s) %d days ago"%(obj["players"][0]["NumberOfVACBans"],obj["players"][0]["DaysSinceLastBan"])
+			vac[steamID]["data"] = "\033[1;31m%d VAC ban(s) %d days ago\033[1;m"%(obj["players"][0]["NumberOfVACBans"],obj["players"][0]["DaysSinceLastBan"])
 			vac[steamID]["time"] = time.time()
+			subprocess.check_output(["easyrule", "block WAN %s"%ip], 1)
+			print("\033[1;31m%s BANNED - VAC\033[1;m"%ip)
 			return "%d VAC ban(s) %d days ago"%(obj["players"][0]["NumberOfVACBans"],obj["players"][0]["DaysSinceLastBan"])
 	else:
 		return vac.get(steamID).get("data")
 
 hours = {}
+
 def gameCheck(steamID):
 	if hours.get(steamID) is None:
 		hours[steamID] = {"data":"", "time":0}
-	if hours.get(steamID).get("time") > time.time() - 3600:
+	if hours.get(steamID).get("time") < time.time() - 3600:
 		steamKey = open('steamAPI.txt').read()
 		url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&format=json"%(steamKey, steamID)
 		req = request.urlopen(url)
 		encoding = req.headers.get_content_charset()
 		obj = json.loads(req.read().decode(encoding))
-		print(url)
 		if obj.get("response") is not None:
-			if obj.get("game") is not None:
+			if obj["response"].get("games") is not None:
 				for each in obj["response"]["games"]:
 					if each["appid"] == 10190:
 						hours[steamID]["data"] = int(each.get("playtime_forever")/60)
 						hours[steamID]["time"] = time.time()
 						return int(each.get("playtime_forever")/60)
 			else:
+				print(url)
 				return "private"
 		else:
+			print(url)
 			return "private"
 	else:
 		return hours.get(steamID).get("data")
@@ -99,7 +121,7 @@ def customAction(data1):
 				unpackedPacket = struct.unpack('<b3s%ds5xqIIhh24xIIxhbbbq'%nameLength, a)
 				if sanityCheck(unpackedPacket):
 					user = userLookup(str(dec2ip(unpackedPacket[5])))
-					table.append([unpackedPacket[0], unpackedPacket[2], "%s:%d"%(dec2ip(unpackedPacket[5]),unpackedPacket[7]), "%s:%d"%(dec2ip(unpackedPacket[4]),unpackedPacket[6]), unpackedPacket[3], unpackedPacket[10], unpackedPacket[11], unpackedPacket[12], unpackedPacket[13], vacCheck(unpackedPacket[3]), gameCheck(unpackedPacket[3]), "%s, %s, %s" % (user.city, user.region, user.country)])
+					table.append([unpackedPacket[0], unpackedPacket[2], "%s:%d"%(dec2ip(unpackedPacket[5]),unpackedPacket[7]), "%s:%d"%(dec2ip(unpackedPacket[4]),unpackedPacket[6]), unpackedPacket[3], unpackedPacket[10], unpackedPacket[11], unpackedPacket[12], unpackedPacket[13], vacCheck(unpackedPacket[3],dec2ip(unpackedPacket[5])), gameCheck(unpackedPacket[3]), "%s, %s, %s" % (user.city, user.region, user.country)])
 					offset = offset + length
 				else:
 					result = re.search(b'.+?\x00{24}.....................', data[offset:], re.DOTALL)
@@ -109,7 +131,7 @@ def customAction(data1):
 					unpackedPacket = struct.unpack('<b3s%ds5xqIIhh24xIIhbbbq'%nameLength, a)
 					if sanityCheck(unpackedPacket):
 						user = userLookup(str(dec2ip(unpackedPacket[5])))
-						table.append([unpackedPacket[0], unpackedPacket[2], "%s:%d"%(dec2ip(unpackedPacket[5]),unpackedPacket[7]), "%s:%d"%(dec2ip(unpackedPacket[4]),unpackedPacket[6]), unpackedPacket[3], unpackedPacket[10], unpackedPacket[11], unpackedPacket[12], unpackedPacket[13], vacCheck(unpackedPacket[3]), gameCheck(unpackedPacket[3]), "%s, %s, %s" % (user.city, user.region, user.country)])
+						table.append([unpackedPacket[0], unpackedPacket[2], "%s:%d"%(dec2ip(unpackedPacket[5]),unpackedPacket[7]), "%s:%d"%(dec2ip(unpackedPacket[4]),unpackedPacket[6]), unpackedPacket[3], unpackedPacket[10], unpackedPacket[11], unpackedPacket[12], unpackedPacket[13], vacCheck(unpackedPacket[3],dec2ip(unpackedPacket[5])), gameCheck(unpackedPacket[3]), "%s, %s, %s" % (user.city, user.region, user.country)])
 						offset = offset + length
 			print(tabulate(table))
 			#print(tabulate(table, headers="firstrow"))
